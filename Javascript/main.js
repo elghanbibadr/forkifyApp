c71c525e68f603674abae7014a283833738449b1
badr ghanbi <bghanbi50@gmail.com>
2022-11-22 15:03:58 +0100

    more funcionality

--- Javascript/main.js
+++ Javascript/main.js
@@ -14,6 +14,8 @@ let ingredientInfo=document.querySelector('.ingredientInfo')
 let recipes;
 let ingredientList=document.querySelector('.ingredient-list');
 let btnController=document.querySelector('.btn-controler-box');
+let recipeToBeVisiblePerPage=7;
+let count=0;
 // FUNCTIONS
 const clearRecipeResultContainer=()=>recipeResultContainer.innerHTML=0;
 
@@ -39,7 +41,8 @@ const showRecipies=({recipes:results})=>{
  results.forEach((result,index)=>{
     let {publisher,title,recipe_id,image_url}=result;
      recipeBox=document.createElement('div');
-    recipeBox.classList.add('recipe','hidden');
+    recipeBox.classList.add('recipe');
+    recipeBox.classList.add('hidden');
     recipeBox.id=recipe_id;
     recipeBox.setAttribute('recipe-index',index);
     recipeBox.innerHTML=`
@@ -50,16 +53,19 @@ const showRecipies=({recipes:results})=>{
     </div>
     `;
     btnController.classList.remove('hidden');
-    getFirstPageResult(recipeBox,count)
+    splitResults(recipeBox,count)
     recipeResultContainer.append(recipeBox)    
     recipeBox.addEventListener('click',getIngredients)
     }); 
 }
 
-const getFirstPageResult=(elements,total)=>{
-  if (+recipeBox.getAttribute('recipe-index') < total/4 ){
-      recipeBox.classList.remove('hidden');
+const splitResults=(element,total)=>{
+  if ((+element.getAttribute('recipe-index') >count) &&  (+element.getAttribute('recipe-index') < recipeToBeVisiblePerPage)){
+      element.classList.remove('hidden');
   }
+}
+const getNextPageResult=(e)=>{
+    console.log(e.target)
 }
 
 const getResult=async(recipeName)=>{
@@ -97,12 +103,10 @@ const showIngredient=(items)=>{
      ingredientList.append(li)
     })
   
-    console.log(ingredientList)
 
 }
 
 async function getIngredients(){
-    console.log(searchMsgBox)
     searchMsgBox.classList.add('hidden')
         showLoader(loader2)
       let recipeId=this.id;
@@ -125,8 +129,7 @@ searchBtn.addEventListener('click',(e)=>{
 })
 
 
-
-
+btnController.addEventListener('click',getNextPageResult)
 
 
 
